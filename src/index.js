79e0a0e58debc9cc3b3d206609fc66e24b6538b2
StanislavKuchma <kuchmastanislav73@gmail.com>
2022-07-06 18:20:50 +0300

    fix10

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="">
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyles} />
         <App />
